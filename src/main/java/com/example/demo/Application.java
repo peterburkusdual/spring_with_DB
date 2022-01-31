14c68e440fd639b911482bf5a96193f4aaa683c4
peterburkusdual <yourschief1@gmail.com>
2022-01-31 10:30:14 +0100

    Uploading finished code by tutorial

---
+++ src/main/java/com/example/demo/Application.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+        return args -> {
+            Student maria = new Student(
+                    "Maria",
+                    "Jones",
+                    "maria.jones@gmial.com",
+                    25
+            );
+            studentRepository.save(new Student());
+        };
+    }
+}
